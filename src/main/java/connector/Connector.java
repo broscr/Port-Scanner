32abb79a5b79c43d31ba478c6743babb5996a3b2
RIDVAN <ridvanaktepe@hotmail.com>
2022-06-27 13:27:38 +0300

    port detail search disabled

--- src/main/java/connector/Connector.java
+++ src/main/java/connector/Connector.java
@@ -6,39 +6,44 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Connector {
 
     private final int TIMEOUT = 1000;
-    private final int THREAD_SIZE = 100;
     private final String ip;
     private final int[] RANGE;
 
     public Connector(String ip, int startPort, int endPort) {
+        int MAX_RANGE = 65000;
+
         this.ip = ip;
-        RANGE = IntStream.range(startPort, endPort).toArray();
+        RANGE = IntStream.range(startPort, Math.min(endPort, MAX_RANGE)).toArray();
     }
 
-    private void portConnect(ExecutorService executorService, String ip, int port) {
-        executorService.submit(() -> {
+    private Future<Status> portConnect(ExecutorService executorService, String ip, int port) {
+        return executorService.submit(() -> {
 
             Status status = new Status();
+
             try {
+
                 Socket socket = new Socket();
                 socket.connect(new InetSocketAddress(ip, port), TIMEOUT);
+//                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//                String result = reader.readLine();
 
-                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String result = reader.readLine();
-
-                reader.close();
+//                reader.close();
                 socket.close();
 
                 status.setPort(port);
                 status.setOpen(true);
-                System.out.println("Connected:  " + port + "\t\t" + result);
+                System.out.println("Connected:  " + port /*+ "\t\t" + result*/);
                 return status;
             } catch (Exception e) {
 //                System.out.println("Error : " + port + " : " + e.getMessage());
@@ -47,15 +52,48 @@ public class Connector {
                 return status;
             }
         });
-
-
     }
 
     public void portControl() {
+        long startMillis = System.currentTimeMillis();
+        int THREAD_SIZE = 100;
+
+        List<Future<Status>> futureList = new ArrayList<>();
+
         final ExecutorService executorService = Executors.newFixedThreadPool(THREAD_SIZE);
+
         for (int port : RANGE) {
-            portConnect(executorService, ip, port);
+            futureList.add(portConnect(executorService, ip, port));
         }
         executorService.shutdown();
+
+        List<Status> statusList = futureList.stream().map(i -> {
+            try {
+                return new Status(i.get().getPort(), i.get().isOpen());
+            } catch (InterruptedException | ExecutionException e) {
+                return null;
+            }
+        }).collect(Collectors.toList());
+
+        statusList.forEach(i -> {
+            if (i.isOpen()) System.out.println(i);
+        });
+
+        showTotalTime(System.currentTimeMillis() - startMillis);
+    }
+
+    private void showTotalTime(long milliseconds) {
+        // formula for conversion for
+        // milliseconds to minutes.
+        long minutes = (milliseconds / 1000) / 60;
+
+        // formula for conversion for
+        // milliseconds to seconds
+        long seconds = (milliseconds / 1000) % 60;
+
+        // Print the output
+        System.out.println(milliseconds + " Milliseconds = "
+                + minutes + " minutes and "
+                + seconds + " seconds.");
     }
 }
